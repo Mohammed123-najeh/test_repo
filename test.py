06123945eec11d60e66b4e082bb18b1b675ca295
Mohammed Najeh <mohammed.xxmoxx@gmail.com>
2025-08-01 09:57:13 -0700

    new function add summation

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
-x =["hi",3,6]
-print(x)
-x.pop()
-print(x)
+def summation(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+
+# Example usage:
+print(summation([1, 2, 3, 4, 5]))  # Output: 15
